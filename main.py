b53312d2371fc83ca9c9578224af937912e89083
Brenda <102390271+brems1@users.noreply.github.com>
2022-04-02 21:07:13 +0000

    It didn't work when I would run it in this assignment but it worked when I did it in the other one. So, I just copied and pasted it here so that I can submit it. I tried to do the bonus of verifying that the guess is valid but I couldn't think of it.

--- main.py
+++ main.py
@@ -2,11 +2,28 @@
   Starter Code for number guessing game
   
   Please enter your team members names here:
-  name1
-  name2
+brenda m.
 '''
 
 import random
 answer = random.randint(1, 100)
 
 # Begin your code here
+
+x = input('What is your name: ')
+print('Hi, ' + x + '!' + '\n' + '\t' + 'Please guess a number from 1 to 100.')
+
+y = int(input('Your ' + 'guess: '))
+
+while answer != 'y':
+  print
+  if y > answer:
+    print('Too High') 
+    y = int(input('Guess again, ' + x + ':'))
+  elif y < answer:
+    print('Too Low') 
+    y = int(input('Guess again, ' + x + ':'))
+  else:
+    print('Win')
+    break
+    print
